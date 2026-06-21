a307657ba19126cf06e8647e34394b5ebe60749d
ma1gnan <119422502+ma1gnan@users.noreply.github.com>
2026-06-21 14:04:55 -0400

    Add files via upload

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
+admin.initializeApp();
+
+//  Sample Cloud Function
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
